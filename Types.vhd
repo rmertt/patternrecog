81ca192cf3a4dfbe858fd88a099b842bc8d0319b
Recep Mert Ulukaya <68912260+rmertt@users.noreply.github.com>
2025-06-17 11:20:16 +0300

    Update Types.vhd

--- Types.vhd
+++ Types.vhd
@@ -4,7 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 package Types is
 
-  -- Ortak türler
+  -- common types
   
   subtype pixel_value is integer range 0 to 255;
   type integer_vector is array (natural range <>) of pixel_value;
@@ -13,7 +13,7 @@ package Types is
 
 
 
-  -- Tüm nöronlar için ortak ağırlık boyutları
+  -- weight dimensions for all neurons
   type weight_array_784 is array (0 to 783) of integer;  -- Layer 1
   type weight_array_128 is array (0 to 127) of integer;  -- Layer 4
   type weight_array_64  is array (0 to 63)  of integer;  -- Layer 6
@@ -22,7 +22,7 @@ package Types is
   type weight_matrix_layer4_type is array (0 to 63)  of weight_array_128;
   type weight_matrix_layer6_type is array (0 to 9)   of weight_array_64;
 
-  -- Tüm bias dizileri
+  -- all bias series
   subtype bias_array_layer1 is integer_vector(0 to 127);
   subtype bias_array_layer4 is integer_vector(0 to 63);
   subtype bias_array_layer6 is integer_vector(0 to 9);
